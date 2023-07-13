e2b956b7e571e68372674345dccb1b0bab2c2c63
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-07-13 14:02:38 -0400

    added some code to login component

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -10,14 +10,34 @@ function LoginForm() {
     e.preventDefault()
 
     // add async stuff
-
+    fetch(`/`)
     setUsername('')
     setPassword('')
   }
 
   return (
-
-    <></>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">Login</button>
+      {/* <button type="submit">Signup</button> */}
+    </form>
   )
 }
 
